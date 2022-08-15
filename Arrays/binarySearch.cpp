eaa05a5025a2ae4e8c5b602794d1f72488ec1a86
Sakshi Thakur <nehathakursakshi@gmail.com>
2022-08-15 03:48:05 -0700

    Create binarySearch.cpp

---
+++ Arrays/binarySearch.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        int mid=0,l=0,r=n-1;
+        while(l<=r){
+            mid = (l+r)/2;
+            if(arr[mid] == k)
+                return mid;
+            else if(arr[mid] > k){
+                r=mid-1;
+            }
+            else
+            l=mid+1;
+        }
+        return -1;
+    }
+};
