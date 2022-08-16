480124f3ecbefc9ebdd2e25ef4c4360b32577def
Sakshi Thakur <nehathakursakshi@gmail.com>
2022-08-16 06:55:19 -0700

    Create minDistanceBetweenTwoNumbers.cpp

---
+++ Arrays/minDistanceBetweenTwoNumbers.cpp
@@ -0,0 +1,19 @@
+ int minDist(int a[], int n, int x, int y) {
+        // code here
+         int p = -1, min_dist = INT_MAX;
+    
+    for(int i=0 ; i<n ; i++)
+    {
+        if(arr[i]==x || arr[i]==y)
+        {
+            if( p != -1 && arr[i] != arr[p])
+                min_dist = min(min_dist , i-p);
+                p=i;
+        }
+    }
+    //If distance is equal to int max 
+    if(min_dist==INT_MAX)
+        return -1;
+
+    return min_dist;
+    }
