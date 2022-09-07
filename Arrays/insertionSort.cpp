bb936f73b7a47fa299ad170a031843d0fcb73eca
Sakshi Thakur <nehathakursakshi@gmail.com>
2022-09-07 06:36:48 -0700

    Update insertionSort.cpp

--- Arrays/insertionSort.cpp
+++ Arrays/insertionSort.cpp
@@ -1,3 +1,12 @@
+/*
+Approach
+In insertion sort, the basic idea is to divide the array into a sorted and an unsorted part. Elements from the unsorted part are picked one by one and placed at their correct position in the sorted part until the whole array is sorted.
+
+Iterate the array from i = 1 to i < n. Compare the i-th element to its previous elements one by one until it is less than its previous elements and keep moving the greater elements forward.
+Once the correct position for the i-th element is found (i.e. i-th element is greater than its previous element), place it at that position.
+*/
+
+
 void insertionSort(vector<int> &arr) {
 	int n=arr.size();
 	 int i, key, j; 
