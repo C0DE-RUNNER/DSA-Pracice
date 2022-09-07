3c11b60e25ca0edb3b7d58ea4712e792aa294b4a
Sakshi Thakur <nehathakursakshi@gmail.com>
2022-09-07 06:15:25 -0700

    Create getEvenDigitNumbers.cpp

---
+++ Arrays/getEvenDigitNumbers.cpp
@@ -0,0 +1,7 @@
+/*
+Given an array of integers, find the elements which have an even number of digits.
+
+Example
+Array: [42, 564, 5775, 34, 123, 454, 1, 5, 45, 3556, 23442]
+Answer: 42, 5775, 34, 45, 3556
+*/
