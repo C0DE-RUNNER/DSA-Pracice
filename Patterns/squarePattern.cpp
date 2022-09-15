7f953b1df55c4e7a3fd436efd645a33f7bdb0738
Sakshi Thakur <nehathakursakshi@gmail.com>
2022-09-15 00:27:52 -0700

    Update squarePattern.cpp

--- Patterns/squarePattern.cpp
+++ Patterns/squarePattern.cpp
@@ -27,3 +27,16 @@ Constraints:
 */
 
 
+class Solution{
+public:
+	
+	void printSquare(int n) {
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<n;j++){
+	            cout<<"* ";
+	        }
+	        cout<<endl;
+	    }
+	    // code here
+	}
+};
